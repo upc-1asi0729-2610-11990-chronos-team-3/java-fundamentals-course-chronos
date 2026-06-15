99b15177162de7279aa16bfa81dce9c869507d8f
angelfdevs <angelfloreseusebio@gmail.com>
2026-06-15 08:18:27 -0500

    Create Bucle-While.java

---
+++ completed-examples/lecture-3/Bucle-While.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+}
